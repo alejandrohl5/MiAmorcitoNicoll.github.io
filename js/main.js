15c33b74959e7a7338a996abe3b9e2b7a569fa9a
alejandrohl5 <76087583@certus.edu.pe>
2022-10-09 21:51:55 -0500

    mi nicoll

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const typed = new Typed('.card__parrafo', {
+    strings: ['Eres lo mejor que me está pasando en mi vida mi amor, te amo mucho mi Nicoll <br> ! FELIZ ANIVERSARIO MI AMOR! <br>10/09/2022'],
+    typeSpeed:100,
+    backSpeed:60,
+    backDelay:1000,
+    showCursor:false,
+    loop: true
+} );
